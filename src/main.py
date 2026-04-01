fae401efbec2f3d3e98f1beea4f88cc0b3ed7ce5
ReeseHatfield <reesehatfield02@gmail.com>
2026-04-01 18:50:05 -0400

    motion detection

--- src/main.py
+++ src/main.py
@@ -3,8 +3,8 @@ import time
 import requests
 import os
 
-CHANNEL_ID = "1224514100210569327"
 
+# I hate this, but i feel like it might be the best way sadly
 def find_cam_index() -> int:
     for i in range(6):
         test_cap = cv2.VideoCapture(i)
@@ -21,12 +21,87 @@ def load_token() -> str:
         
         return line.split("TOKEN=")[-1]
 
+
+def check_time(last_capture) -> bool:
+    now = time.time()
+    return now - last_capture > 30
+
+
+def clear_cap_buffer(cap) -> None:
+    for _ in range(5): cap.grab()
     
+
+def take_pic(cap) -> str:
+    clear_cap_buffer(cap);
+    
+    _, img = cap.retrieve()
+    
+    path = "snap.jpg"
+    cv2.imwrite(path, img)
+    
+    return path
+
+
+def build_header():
+    return {
+        "Authorization": f"Bot {BOT_TOKEN}"
+    }
+
+
+def send_image_to_discord(img_path: str, headers): 
+    
+    with open(img_path, "rb") as f:
+        url = f"https://discord.com/api/v10/channels/{CHANNEL_ID}/messages"
+        r = requests.post(url, headers=headers, files={"file": f})
+        if r.status_code == 200:
+            print("Sent to Discord.")
+        else:
+            print(f"Error: {r.status_code} - {r.text}")
+
+
+# todo read me in
+CHANNEL_ID = "1224514100210569327"
+BOT_TOKEN = load_token()
+CAM_INDEX = find_cam_index()
+FRAME_DIFF_SENSITIVITY_PX = 50_000
+
 def main() -> None:
     
-    BOT_TOKEN = load_token()
-    CAM_INDEX = find_cam_index()
     cap = cv2.VideoCapture(CAM_INDEX)
     
+    last_capture = 0
+
+    headers = build_header()
+
+    print("monitoring for motion...")
+
+    while True:
+        ret, frame1 = cap.read()
+        time.sleep(0.1) 
+        ret, frame2 = cap.read()
+
+        if not ret:
+            break
+
+        # check motion
+        frame_diff = cv2.absdiff(frame1, frame2)
+        flat_diff_no_color = cv2.cvtColor(frame_diff, cv2.COLOR_BGR2GRAY) 
+        if cv2.countNonZero(flat_diff_no_color) < FRAME_DIFF_SENSITIVITY_PX:
+            continue
+        
+        
+        if check_time(last_capture):
+            print("Motion Detected. Taking pic...")
+            time.sleep(2)
+            
+            img_path = take_pic(cap)
+            
+            send_image_to_discord(img_path, headers)
+            
+            last_capture = time.time()
+
+    cap.release()
+    
+
 if __name__ == "__main__":
     main()
