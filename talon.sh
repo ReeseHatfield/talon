8ff5b641c8cce9fda9dbacf45338105cb0036ef6
ReeseHatfield <reesehatfield02@gmail.com>
2026-05-05 08:45:37 -0400

    add python finder

--- talon.sh
+++ talon.sh
@@ -5,12 +5,18 @@ git pull --rebase
 pip install --upgrade pip
 
 
+if command -v python3.12 >/dev/null 2>&1; then
+    PYTHON=python3.12
+else
+    PYTHON=python3
+fi
+
 if [ ! -d "venv" ]; then
-    python3 -m venv venv
+    $PYTHON -m venv venv
 fi
 
 source venv/bin/activate
 
 pip install -r requirements.txt
 
-nohup python3 -u src/main.py > talon.out &
+nohup $PYTHON -u src/main.py > talon.out &
